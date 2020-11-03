fc2723d2dedaf36b2493f9db7f1ea0307ea7ac30
AmirKAT <amir-katal@hotmail.com>
2020-11-03 00:24:16 +0000

    Add Bootstrap and list 10 lessons v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container my-5">
+    <h1>My online store</h1>    
+    
+    <!-- displaying classes using v-for -->
+    <div class="row">
+      <div class="col-md-3" v-for="item in classes" :key="item.subject">
+        {{ item. subject }}
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data() {
+    return {
+      classes: [
+        { subject: 'Maths', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'English', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'French', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'ICT', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Economics', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Art', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Cooking', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Biology', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Chemistry', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Physics', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+      ],
+    };
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* Nothing for now */
 </style>
