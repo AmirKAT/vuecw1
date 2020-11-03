516e011455feb7b8191dfa85e815234599d1afb6
AmirKAT <amir-katal@hotmail.com>
2020-11-03 23:47:15 +0000

    Add images and css to classes v1.1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app" class="container my-5">
-    <h1>My online store</h1>    
+    <h1>AK's Online Classes</h1>    
     
     <!-- displaying classes using v-for -->
     <div class="row">
       <div class="col-md-3" v-for="item in classes" :key="item.subject">
-        {{ item. subject }}
+
+        <!-- adding css using bootstrap 4 to organise classes -->
+        <div class="card">
+          <img :src="item.image" class="card-img-top">
+          <div class="card-body">
+            <h4 class="card-title">{{ item.subject }}</h4>
+            <div class="card-text">Location: {{ item.location }}</div>
+            <div class="card-text">£{{ item.price }}</div>
+            <div class="card-text">Spaces left: {{ item.spaces }}</div>
+            <div class="row justify-content-end">
+              <button class="btn btn-primary">Add to cart</button>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
-
-  </div>
+    
+  </div> 
 </template>
 
 <script>
@@ -18,16 +32,56 @@ export default {
   data() {
     return {
       classes: [
-        { subject: 'Maths', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'English', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'French', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'ICT', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Economics', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Art', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Cooking', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Biology', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Chemistry', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
-        { subject: 'Physics', location: 'Zoom Link', spaces: 5, image: '//placehold.it/200', price: 10 },
+        { subject: 'Maths', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://www.learnpad.com/wp-content/uploads/2018/10/maths-1.png' , 
+        price: 10 },
+        { subject: 'English', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://www.amysenglish.com/wp-content/uploads/2020/07/logo_piccolo.png', 
+        price: 15 },
+        { subject: 'French', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://mylingotrip.com/wp-content/uploads/2018/12/france.png', 
+        price: 15 },
+        { subject: 'ICT', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://digitalsignagesummit.org/europe/wp-content/uploads/2019/06/ICT-Logo-200x200-1.jpg', 
+        price: 20 },
+        { subject: 'Economics', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://i.pinimg.com/originals/69/b8/5b/69b85b67df27c2f4f3abe81e06faf125.png', 
+        price: 20 },
+        { subject: 'Art', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://i.pinimg.com/236x/82/96/8e/82968e121cde49504b8afbe7dffb1aa8.jpg', 
+        price: 5 },
+        { subject: 'Cooking', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://www.cookeryschool.co.uk/wp-content/uploads/2018/03/180308-Cookery-School-LR-18-e1522247425632.jpg', 
+        price: 25 },
+        { subject: 'Biology', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://static.vecteezy.com/system/resources/thumbnails/000/473/371/small/25._Biology_Conceptual_Design.jpg', 
+        price: 15 },
+        { subject: 'Chemistry', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://icon-library.com/images/chemistry-icon/chemistry-icon-16.jpg', 
+        price: 15 },
+        { subject: 'Physics', 
+        location: 'Zoom Link', 
+        spaces: 5, 
+        image: 'https://wp-media.petersons.com/testprep/wp-content/uploads/2018/10/08173528/physics.png', 
+        price: 15 },
       ],
     };
   },
