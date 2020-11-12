36064ad05f207170ce3cd07196f90d0f7e62ad9d
AmirKAT <amir-katal@hotmail.com>
2020-11-12 16:48:43 +0000

    Add 4 sort methods v1.12

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,29 @@
     <!-- The below div tag holds the page that has lessons for sale -->
     <div v-if="showLessons">
 
-      <div class="text-right">
+      <div class="float-left">
         <!-- buttons to sort classes by price -->
-        <button class="btn btn-primary btn-sm" @click="sortLow">Sort by lowest price</button>
-        <button class="btn btn-primary btn-sm" @click="sortHigh">Sort by highest price</button>
+        <p>Price</p>
+        <button class="btn btn-primary btn-sm" @click="sortLowPrice()">Ascending</button>
+        <button class="btn btn-primary btn-sm" @click="sortHighPrice()">Descending</button>
+        <br>
+        <br>
+        <!-- buttons to sort classes by location -->
+        <p>Distance</p>
+        <button class="btn btn-primary btn-sm" @click="sortLowLocation()">Ascending</button>
+        <button class="btn btn-primary btn-sm" @click="sortHighLocation()">Descending</button>
+        <br>
+        <br>
+        <!-- buttons to sort classes by spaces available -->
+        <p>Spaces</p>
+        <button class="btn btn-primary btn-sm" @click="sortLowSpace()">Ascending</button>
+        <button class="btn btn-primary btn-sm" @click="sortHighSpace()">Descending</button>
+        <br>
+        <br>
+        <!-- buttons to sort classes by Subject -->
+        <p>Subject</p>
+        <button class="btn btn-primary btn-sm" @click="sortLowSubject()">Ascending</button>
+        <button class="btn btn-primary btn-sm" @click="sortHighSubject()">Descending</button>
       </div>
 
       <!-- space between buttons and classes -->
@@ -32,15 +51,15 @@
 
       <!-- displaying classes using v-for -->
       <div class="row">
-        <div class="col-md-3" v-for="(product, index) in classes" :key="index">
+        <div class="col-md-4" v-for="(product, index) in classes" :key="index">
 
           <!-- adding css using bootstrap 4 to organise classes -->
           <div class="card">
             <img :src="product.image" class="card-img-top">
             <div class="card-body">
               <h4 class="card-title">{{ product.subject }}</h4>
-              <div class="card-text">Location: {{ product.location }}</div>
-              <div class="card-text">£{{ product.price }}</div>
+              <div class="card-text">Distance in Km: {{ product.location }}</div>
+              <div class="card-text">Price: £{{ product.price }}</div>
               <div class="card-text">Spaces left: {{ product.spaces }}</div>
               <div class="row justify-content-end">
                 <!-- the add to cart button becomes disabled once the spaces reach 0 -->
@@ -151,7 +170,7 @@ export default ({
         {
         id: 1001,
         subject: 'Maths',
-        location: 'Zoom Link',
+        location: 3,
         spaces: 5,
         image: 'https://www.learnpad.com/wp-content/uploads/2018/10/maths-1.png' ,
         price: 10 
@@ -159,7 +178,7 @@ export default ({
         {
         id: 1002,
         subject: 'English',
-        location: 'Zoom Link',
+        location: 8,
         spaces: 5,
         image: 'https://www.amysenglish.com/wp-content/uploads/2020/07/logo_piccolo.png',
         price: 15 
@@ -167,7 +186,7 @@ export default ({
         {
         id: 1003,
         subject: 'French',
-        location: 'Zoom Link',
+        location: 10,
         spaces: 5,
         image: 'https://mylingotrip.com/wp-content/uploads/2018/12/france.png',
         price: 15 
@@ -175,7 +194,7 @@ export default ({
         {
         id: 1004,
         subject: 'ICT',
-        location: 'Zoom Link',
+        location: 5,
         spaces: 5,
         image: 'https://digitalsignagesummit.org/europe/wp-content/uploads/2019/06/ICT-Logo-200x200-1.jpg',
         price: 20 
@@ -183,7 +202,7 @@ export default ({
         {
         id: 1005,
         subject: 'Economics',
-        location: 'Zoom Link',
+        location: 6,
         spaces: 5,
         image: 'https://i.pinimg.com/originals/69/b8/5b/69b85b67df27c2f4f3abe81e06faf125.png',
         price: 20 
@@ -191,7 +210,7 @@ export default ({
         {
         id: 1006, 
         subject: 'Art',
-        location: 'Zoom Link',
+        location: 4,
         spaces: 5,
         image: 'https://i.pinimg.com/236x/82/96/8e/82968e121cde49504b8afbe7dffb1aa8.jpg',
         price: 5 
@@ -199,7 +218,7 @@ export default ({
         {
         id: 1007,
         subject: 'Cooking',
-        location: 'Zoom Link',
+        location: 14,
         spaces: 5,
         image: 'https://www.cookeryschool.co.uk/wp-content/uploads/2018/03/180308-Cookery-School-LR-18-e1522247425632.jpg',
         price: 25 
@@ -207,7 +226,7 @@ export default ({
         {
         id: 1008, 
         subject: 'Biology',
-        location: 'Zoom Link',
+        location: 7,
         spaces: 5,
         image: 'https://static.vecteezy.com/system/resources/thumbnails/000/473/371/small/25._Biology_Conceptual_Design.jpg',
         price: 15 
@@ -215,7 +234,7 @@ export default ({
         {
         id: 1009, 
         subject: 'Chemistry',
-        location: 'Zoom Link',
+        location: 12,
         spaces: 5,
         image: 'https://icon-library.com/images/chemistry-icon/chemistry-icon-16.jpg',
         price: 15 
@@ -223,7 +242,7 @@ export default ({
         {
         id: 1010, 
         subject: 'Physics',
-        location: 'Zoom Link',
+        location: 10,
         spaces: 5,
         image: 'https://wp-media.petersons.com/testprep/wp-content/uploads/2018/10/08173528/physics.png',
         price: 15 
@@ -236,13 +255,37 @@ export default ({
 
   methods: {
 
-    sortHigh() {
+    sortHighPrice() {
       this.classes.sort((a, b) => a.price < b.price ? 1 : -1);
-    }, //end of sortHigh
+    }, //end of sortHighPrice
 
-    sortLow() {
+    sortLowPrice() {
       this.classes.sort((a, b) => a.price > b.price ? 1 : -1);
-    }, //end of sortLow 
+    }, //end of sortLowPrice
+
+    sortHighLocation() {
+      this.classes.sort((a, b) => a.location < b.location ? 1 : -1);
+    }, //end of sortHighPrice
+
+    sortLowLocation() {
+      this.classes.sort((a, b) => a.location > b.location ? 1 : -1);
+    }, //end of sortLowPrice
+
+    sortHighSpace() {
+      this.classes.sort((a, b) => a.spaces < b.spaces ? 1 : -1);
+    }, //end of sortHighSpace
+
+    sortLowSpace() {
+      this.classes.sort((a, b) => a.spaces > b.spaces ? 1 : -1);
+    }, //end of sortLowSpace
+
+    sortHighSubject() {
+      this.classes.sort((a, b) => a.subject < b.subject ? 1 : -1);
+    }, //end of sortHighSpace
+
+    sortLowSubject() {
+      this.classes.sort((a, b) => a.subject > b.subject ? 1 : -1);
+    }, //end of sortLowSpace
 
     decrement(n) {
       n.spaces -= 1;
